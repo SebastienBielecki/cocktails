c1d3b290289baede834ef43f5f02e7e0ba19d9d2
Sebastien <seb_bielecki@hotmail.com>
2022-09-08 17:41:30 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor diam, dictum ut nunc ut, iaculis aliquam leo. Proin mollis libero ac lorem ullamcorper mollis. Ut sed ex ut ante scelerisque ultrices. Donec lacinia neque fringilla elementum consequat. Sed placerat.
+      </p>
+    </section>
+  )
+}
+
+export default About
